8d924d1f3de8a30de3f4229435cbc110995ea452
Evan Bouman <evan.bouman@gmail.com>
2024-10-24 11:06:16 -0500

    Fixed issue with delete

--- src/components/ShelfPage/ShelfPage.jsx
+++ src/components/ShelfPage/ShelfPage.jsx
@@ -1,61 +1,58 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function ShelfPage() {
   const dispatch = useDispatch();
-  const items = useSelector((store)=> store.shelfItems)
-  const user = useSelector((store) => store.user)
+  const items = useSelector((store) => store.shelfItems);
+  const user = useSelector((store) => store.user);
 
-  const [descriptionInput, setDescriptionInput] = useState('')
-  const [imageInput, setImageInput] = useState('')
-  
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [imageInput, setImageInput] = useState("");
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_SHELF" });
+  }, []);
+
   const deleteItem = (item) => {
     let data = {
       id: item.id,
-      user_id: item.user_id 
-    }
-    dispatch({type: 'DELETE_ITEM', payload: data})
-  }
-
-  useEffect(()=>{
-    dispatch({type: 'FETCH_SHELF'})
-  }, [])
-  
-  // const postItem = (item) => {
-  //   dispatch({type: 'ADD_ITEM', payload: item })
-  // }
+      user_id: item.user_id,
+    };
+    dispatch({ type: "DELETE_ITEM", payload: data });
+  };
 
   const handleImage = (event) => {
-    event.preventDefault()
-    setImageInput(event.target.value)
-  }
+    event.preventDefault();
+    setImageInput(event.target.value);
+  };
 
   const handleDescription = (event) => {
-    event.preventDefault()
-    setDescriptionInput(event.target.value)
-  }
+    event.preventDefault();
+    setDescriptionInput(event.target.value);
+  };
 
-  let data = {
-    description: descriptionInput,
-    image_url: imageInput,
-    user_id: user.id
-  }
+
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    let data = {
+      description: descriptionInput,
+      image_url: imageInput,
+      user_id: user.id,
+    };
     dispatch({
-      type: 'ADD_ITEM',
-      payload: data
-    })
-  }
+      type: "ADD_ITEM",
+      payload: data,
+    });
+  };
 
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      
+
       <table>
         <thead>
           <tr>
@@ -65,36 +62,30 @@ function ShelfPage() {
           </tr>
         </thead>
         <tbody>
-        {items.map(item => {
-        return (
-          <tr key={item.id}>
-            <td>
-              <img src={item.image_url} />
-            </td>
-            <td>
-              <p>{item.description}</p>
-            </td>
-
-            {/* {item.user_id === user.id ? } */}
-            <td>
-              {item.user_id === user.id && <button onClick={deleteItem(item)}>Delete</button>}
-            </td>
-          </tr>
-        )
-      })}
+          {items.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>
+                  <img src={item.image_url} />
+                </td>
+                <td>
+                  <p>{item.description}</p>
+                </td>
+                <td>
+                  {item.user_id === user.id && (
+                    <button onClick={() => deleteItem(item)}>Delete</button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       <form>
-        <input
-        placeholder='Description'
-        onChange={handleDescription} />
-        <input
-        placeholder='Image URL'
-        onChange={handleImage} />
+        <input placeholder="Description" onChange={handleDescription} />
+        <input placeholder="Image URL" onChange={handleImage} />
         <button onClick={handleSubmit}>Add Item</button>
       </form>
-      
-      
     </div>
   );
 }
